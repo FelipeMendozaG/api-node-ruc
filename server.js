132dd8d15704553081b2a1c15815912eaea51033
Jhon Doe <Felipe@bitz.com>
2022-09-17 20:33:50 -0500

    17092022 - servicio para obtener dni

--- server.js
+++ server.js
@@ -24,6 +24,24 @@ app.get('/ruc/:number', async function (req, res) {
     res.json({status:'error'});
   }
 });
+app.get('/dni/:number', async function (req, res) {
+  let number = req.params["number"];
+  if(number){
+    var config = {
+      method: 'get',
+      url: `https://api.apis.net.pe/v1/dni?numero=${number}`,
+      headers: { }
+    };
+    let response = await axios(config)
+    .then(res=>res.data)
+    .catch(function (error) {
+      console.log(error);
+    });
+    res.json(response);
+  }else{
+    res.json({status:'error'});
+  }
+});
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
 
